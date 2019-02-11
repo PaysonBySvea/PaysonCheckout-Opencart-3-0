d6ef4dd4830396b8cf226661cd50982516a2c75e
mickeb <mikael.blom@payson.se>
2019-02-11 12:17:11 +0100

    1.0.0.7 - support for extension installer

--- upload/catalog/controller/extension/payment/paysonCheckout2.php
+++ upload/catalog/controller/extension/payment/paysonCheckout2.php
@@ -4,7 +4,7 @@ class ControllerExtensionPaymentPaysonCheckout2 extends Controller {
     private $testMode;
     public $data = array();
 
-    const MODULE_VERSION = 'paysonEmbedded_1.0.0.5';
+    const MODULE_VERSION = 'paysonEmbedded_1.0.0.7';
 
     function __construct($registry) {
         parent::__construct($registry);
