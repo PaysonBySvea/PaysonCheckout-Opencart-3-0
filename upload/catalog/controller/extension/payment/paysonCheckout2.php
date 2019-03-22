bacc1e57ee12f8081bb90c54b1d3071a543ec02e
oscar-vil <oscar.villegas@payson.se>
2019-03-22 11:42:58 +0100

    ...

--- upload/catalog/controller/extension/payment/paysonCheckout2.php
+++ upload/catalog/controller/extension/payment/paysonCheckout2.php
@@ -3,7 +3,7 @@ class ControllerExtensionPaymentPaysonCheckout2 extends Controller {
     private $testMode;
     public $data = array();
 
-    const MODULE_VERSION = 'paysonEmbedded_1.1.0.5';
+    const MODULE_VERSION = 'paysonEmbedded_1.1.0.6';
 
     function __construct($registry) {
         parent::__construct($registry);
